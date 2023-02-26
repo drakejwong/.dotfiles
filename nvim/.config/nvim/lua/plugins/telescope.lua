465eea267c094fc60a494de36b0bff2f1f67de07
Drake Wong <40375132+drakejwong@users.noreply.github.com>
2023-02-26 04:59:48 -0800

    telescope file browser

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,27 @@
 return {
-  "telescope.nvim",
-  dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    config = function()
-      require("telescope").load_extension("fzf")
-    end,
+  {
+    "telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+      {
+        "nvim-telescope/telescope-file-browser.nvim",
+        keys = {
+          {
+            "<leader>sf",
+            "<cmd>Telescope file_browser path=%:p:h=%:p:h<CR>", -- use path of current file
+            desc = "Browse Files",
+          },
+        },
+        config = function()
+          require("telescope").load_extension("file_browser")
+        end,
+      },
+    },
   },
 }
