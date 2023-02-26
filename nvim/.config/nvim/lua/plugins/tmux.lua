6f8f45eb94651c9822b9bc46d2746679f471b3ff
Drake Wong <40375132+drakejwong@users.noreply.github.com>
2023-02-26 05:35:03 -0800

    tmux which key desc

--- nvim/.config/nvim/lua/plugins/tmux.lua
+++ nvim/.config/nvim/lua/plugins/tmux.lua
@@ -15,49 +15,56 @@ return {
       function()
         require("tmux").move_left()
       end,
+      desc = "Move Left (tmux)",
     },
     {
       "<C-j>",
       function()
         require("tmux").move_bottom()
       end,
+      desc = "Move Down (tmux)",
     },
     {
       "<C-k>",
       function()
         require("tmux").move_top()
       end,
+      desc = "Move Up (tmux)",
     },
     {
       "<C-l>",
       function()
         require("tmux").move_right()
       end,
+      desc = "Move Right (tmux)",
     },
-
     {
       "<M-h>",
       function()
         require("tmux").resize_left()
       end,
+      desc = "Resize Left (tmux)",
     },
     {
       "<M-j>",
       function()
         require("tmux").resize_bottom()
       end,
+      desc = "Resize Down (tmux)",
     },
     {
       "<M-k>",
       function()
         require("tmux").resize_top()
       end,
+      desc = "Resize Up (tmux)",
     },
     {
       "<M-l>",
       function()
         require("tmux").resize_right()
       end,
+      desc = "Resize Right (tmux)",
     },
   },
 }
