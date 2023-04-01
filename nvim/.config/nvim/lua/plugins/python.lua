14c60925b91d3f83945fbc455e8c32d60062963d
Drake Wong <40375132+drakejwong@users.noreply.github.com>
2023-04-01 01:22:59 -0700

    [nvim] py

---
+++ nvim/.config/nvim/lua/plugins/python.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "python",
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      -- stylua: ignore
+      vim.list_extend(opts.ensure_installed, {
+        "blue",
+        "isort",
+        "pyright",
+      })
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      vim.list_extend(opts.sources, {
+        nls.builtins.formatting.blue,
+        nls.builtins.formatting.isort,
+      })
+    end,
+  },
+}
