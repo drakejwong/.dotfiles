025b6484736b967112c2bc7141651d3f6e1ca51e
Drake Wong <40375132+drakejwong@users.noreply.github.com>
2023-06-07 12:49:04 -0700

    wsl clipboard

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd([[
+  let g:clipboard = {
+  \ 'name': 'WSLClipboard',
+  \ 'copy': {
+  \   '+': 'wl-copy',
+  \   '*': 'wl-copy',
+  \ },
+  \ 'paste': {
+  \   '+': 'wl-paste --no-newline',
+  \   '*': 'wl-paste --no-newline',
+  \ },
+  \ 'cache_enabled': 0
+  \ }
+]])
