fe2bf14e347956bbd69ca54adf25b16de7509bb4
Drake Wong <40375132+drakejwong@users.noreply.github.com>
2023-09-04 21:51:57 -0700

    nvim rebase starter

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,17 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.cmd([[
-  let g:clipboard = {
-  \ 'name': 'WSLClipboard',
-  \ 'copy': {
-  \   '+': 'wl-copy',
-  \   '*': 'wl-copy',
-  \ },
-  \ 'paste': {
-  \   '+': 'wl-paste --no-newline',
-  \   '*': 'wl-paste --no-newline',
-  \ },
-  \ 'cache_enabled': 0
-  \ }
-]])
