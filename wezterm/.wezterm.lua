c7f09025e111bf6df12fc91a4a7724362a102635
Drake Wong <40375132+drakejwong@users.noreply.github.com>
2024-02-08 02:44:55 -0800

    [wezterm] color scheme; auto wsl domain

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,7 @@ config.window_frame = {
 	font = wezterm.font({ family = "Hack Nerd Font" }),
 }
 
--- config.color_scheme = "tokyonight"
+config.color_scheme = "tokyonight"
 
 config.window_padding = {
   left = 0,
@@ -15,5 +15,12 @@ config.window_padding = {
   top = 0,
   bottom = 0,
 }
+
+local wsl_domains = wezterm.default_wsl_domains()
+for _, domain in ipairs(wsl_domains) do
+  if domain.name == "WSL:Ubuntu" then
+    config.default_domain = domain.name
+  end
+end
 
 return config
