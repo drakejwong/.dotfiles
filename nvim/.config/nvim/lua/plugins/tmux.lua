4b5552790e026ba2c54c884ddd08fc12cc3896d7
Drake Wong <40375132+drakejwong@users.noreply.github.com>
2024-02-08 10:19:02 -0800

    [nvim,wezterm,tmux] smart splits

--- nvim/.config/nvim/lua/plugins/tmux.lua
+++ nvim/.config/nvim/lua/plugins/tmux.lua
@@ -1,71 +1,72 @@
-return {
-  "aserowy/tmux.nvim",
-  opts = {
-    navigation = {
-      cycle_navigation = false,
-      enable_default_keybindings = false,
-    },
-    resize = {
-      enable_default_keybindings = false,
-    },
-  },
-  keys = {
-    -- TODO: put these in insert mode too
-    {
-      "<C-h>",
-      function()
-        require("tmux").move_left()
-      end,
-      desc = "Move Left (tmux)",
-    },
-    {
-      "<C-j>",
-      function()
-        require("tmux").move_bottom()
-      end,
-      desc = "Move Down (tmux)",
-    },
-    {
-      "<C-k>",
-      function()
-        require("tmux").move_top()
-      end,
-      desc = "Move Up (tmux)",
-    },
-    {
-      "<C-l>",
-      function()
-        require("tmux").move_right()
-      end,
-      desc = "Move Right (tmux)",
-    },
-    {
-      "<M-h>",
-      function()
-        require("tmux").resize_left()
-      end,
-      desc = "Resize Left (tmux)",
-    },
-    {
-      "<M-j>",
-      function()
-        require("tmux").resize_bottom()
-      end,
-      desc = "Resize Down (tmux)",
-    },
-    {
-      "<M-k>",
-      function()
-        require("tmux").resize_top()
-      end,
-      desc = "Resize Up (tmux)",
-    },
-    {
-      "<M-l>",
-      function()
-        require("tmux").resize_right()
-      end,
-      desc = "Resize Right (tmux)",
-    },
-  },
-}
+return {}
+-- return {
+--   "aserowy/tmux.nvim",
+--   opts = {
+--     navigation = {
+--       cycle_navigation = false,
+--       enable_default_keybindings = false,
+--     },
+--     resize = {
+--       enable_default_keybindings = false,
+--     },
+--   },
+--   keys = {
+--     -- TODO: put these in insert mode too
+--     {
+--       "<C-h>",
+--       function()
+--         require("tmux").move_left()
+--       end,
+--       desc = "Move Left (tmux)",
+--     },
+--     {
+--       "<C-j>",
+--       function()
+--         require("tmux").move_bottom()
+--       end,
+--       desc = "Move Down (tmux)",
+--     },
+--     {
+--       "<C-k>",
+--       function()
+--         require("tmux").move_top()
+--       end,
+--       desc = "Move Up (tmux)",
+--     },
+--     {
+--       "<C-l>",
+--       function()
+--         require("tmux").move_right()
+--       end,
+--       desc = "Move Right (tmux)",
+--     },
+--     {
+--       "<M-h>",
+--       function()
+--         require("tmux").resize_left()
+--       end,
+--       desc = "Resize Left (tmux)",
+--     },
+--     {
+--       "<M-j>",
+--       function()
+--         require("tmux").resize_bottom()
+--       end,
+--       desc = "Resize Down (tmux)",
+--     },
+--     {
+--       "<M-k>",
+--       function()
+--         require("tmux").resize_top()
+--       end,
+--       desc = "Resize Up (tmux)",
+--     },
+--     {
+--       "<M-l>",
+--       function()
+--         require("tmux").resize_right()
+--       end,
+--       desc = "Resize Right (tmux)",
+--     },
+--   },
+-- }
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -23,30 +23,71 @@ for _, domain in ipairs(wsl_domains) do
 	end
 end
 
--- tmux
-config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 }
+config.unix_domains = {
+	{
+		name = "wsl",
+		-- Override the default path to match the default on the host win32
+		-- filesystem.  This will allow the host to connect into the WSL
+		-- container.
+		socket_path = "/mnt/c/Users/drake/.local/share/wezterm/sock",
+		-- NTFS permissions will always be "wrong", so skip that check
+		skip_permissions_check = true,
+	},
+}
+
+-- nvim smart splits
+local function is_vim(pane)
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local direction_keys = {
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
+------------------
+------ KEYS ------
+------------------
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
 	-- move
-	{
-		key = "h",
-		mods = "CTRL",
-		action = wezterm.action({ ActivatePaneDirection = "Left" }),
-	},
-	{
-		key = "j",
-		mods = "CTRL",
-		action = wezterm.action({ ActivatePaneDirection = "Down" }),
-	},
-	{
-		key = "k",
-		mods = "CTRL",
-		action = wezterm.action({ ActivatePaneDirection = "Up" }),
-	},
-	{
-		key = "l",
-		mods = "CTRL",
-		action = wezterm.action({ ActivatePaneDirection = "Right" }),
-	},
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	-- resize
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
 	-- swap
 	{
 		key = "{",
