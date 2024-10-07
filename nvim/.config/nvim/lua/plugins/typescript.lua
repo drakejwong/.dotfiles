53911857e73f855ef19e6ff3c9256a57c333e799
Drake Wong <40375132+drakejwong@users.noreply.github.com>
2024-10-07 16:22:13 -0700

    fix: GTFO vtsls

--- nvim/.config/nvim/lua/plugins/typescript.lua
+++ nvim/.config/nvim/lua/plugins/typescript.lua
@@ -28,26 +28,26 @@ return {
       })
     end,
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        vtsls = {
-          root_dir = require('lspconfig.util').root_pattern(".git"),
-          settings = {
-            typescript = {
-              inlayHints = {
-                enumMemberValues = { enabled = false },
-                functionLikeReturnTypes = { enabled = false },
-                parameterNames = { enabled = false },
-                parameterTypes = { enabled = false },
-                propertyDeclarationTypes = { enabled = false },
-                variableTypes = { enabled = false },
-              },
-            }
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       vtsls = {
+  --         root_dir = require('lspconfig.util').root_pattern(".git"),
+  --         settings = {
+  --           typescript = {
+  --             inlayHints = {
+  --               enumMemberValues = { enabled = false },
+  --               functionLikeReturnTypes = { enabled = false },
+  --               parameterNames = { enabled = false },
+  --               parameterTypes = { enabled = false },
+  --               propertyDeclarationTypes = { enabled = false },
+  --               variableTypes = { enabled = false },
+  --             },
+  --           }
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 }
