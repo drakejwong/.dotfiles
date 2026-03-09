233618266e2e64c34974a8aa8d608d679c6e12f3
Drake Wong <40375132+drakejwong@users.noreply.github.com>
2026-03-09 11:56:29 -0700

    refactor(nvim): update plugin sources, fix deprecations
    - vim.loop -> vim.uv (deprecated in newer neovim)
    - williamboman/mason.nvim -> mason-org/mason.nvim (repo rename)
    - echasnovski/mini.surround -> nvim-mini/mini.surround (repo rename)
    - python: blue -> ruff, pyright -> basedpyright
    - python: add ruff format keymap (<leader>cf)
    - example: disable rustaceanvim, update mason source

--- nvim/.config/nvim/lua/plugins/example.lua
+++ nvim/.config/nvim/lua/plugins/example.lua
@@ -1,6 +1,10 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
-if true then return {} end
+-- Disable rust-analyzer
+return {
+  {
+    "mrcjkb/rustaceanvim",
+    enabled = false,
+  },
+}
 
 -- every spec file under config.plugins will be loaded automatically by lazy.nvim
 --
@@ -208,7 +212,7 @@ return {
 
   -- add any tools you want to have installed below
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "stylua",
