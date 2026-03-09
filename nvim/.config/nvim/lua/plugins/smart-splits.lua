e374b941571c8c37ea7c9042e0c320c3bdaf455b
Drake Wong <40375132+drakejwong@users.noreply.github.com>
2026-03-09 11:56:54 -0700

    refactor(nvim): clean up smart-splits config
    - Move at_edge/multiplexer settings to opts (not per-call)
    - Fix resize function names (resize_bottom -> resize_down, resize_top -> resize_up)
    - Remove redundant empty setup() call

--- nvim/.config/nvim/lua/plugins/smart-splits.lua
+++ nvim/.config/nvim/lua/plugins/smart-splits.lua
@@ -2,66 +2,65 @@ return {
   "mrjones2014/smart-splits.nvim",
   lazy = false,
   opts = {
-    config = function()
-      require("smart-splits").setup({
-
-      })
-    end,
+    multiplexer_integration = "wezterm",
+    at_edge = "stop", -- don't wrap within nvim, navigate to wezterm pane instead
+    disable_multiplexer_nav_when_zoomed = false, -- allow navigating out of zoomed pane (unzooms automatically)
   },
   keys = {
     {
       "<C-h>",
       function()
-        require("smart-splits").move_cursor_left({ at_edge = "stop" })
+        require("smart-splits").move_cursor_left()
       end,
       desc = "Move Left (smart splits)",
     },
     {
       "<C-j>",
       function()
-        require("smart-splits").move_cursor_down({ at_edge = "stop" })
+        require("smart-splits").move_cursor_down()
       end,
       desc = "Move Down (smart splits)",
     },
     {
       "<C-k>",
       function()
-        require("smart-splits").move_cursor_up({ at_edge = "stop" })
+        require("smart-splits").move_cursor_up()
       end,
       desc = "Move Up (smart splits)",
     },
     {
       "<C-l>",
       function()
-        require("smart-splits").move_cursor_right({ at_edge = "stop" })
+        require("smart-splits").move_cursor_right()
       end,
       desc = "Move Right (smart splits)",
     },
+
     {
       "<M-h>",
       function()
-        require("smart-splits").resize_left({ at_edge = "stop" })
+        require("smart-splits").resize_left()
       end,
       desc = "Resize Left (smart splits)",
     },
     {
       "<M-j>",
       function()
-        require("smart-splits").resize_bottom({ at_edge = "stop" })
+        require("smart-splits").resize_down()
       end,
       desc = "Resize Down (smart splits)",
     },
     {
       "<M-k>",
       function()
-        require("smart-splits").resize_top({ at_edge = "stop" })
+        require("smart-splits").resize_up()
       end,
       desc = "Resize Up (smart splits)",
     },
     {
       "<M-l>",
       function()
-        require("smart-splits").resize_right({ at_edge = "stop" })
+        require("smart-splits").resize_right()
       end,
       desc = "Resize Right (smart splits)",
     },
